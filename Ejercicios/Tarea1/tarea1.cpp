0530b6e76b12d50604fcaaee3327871eef387bbe
Andrea Rodriguez <arodriguezi@unah.hn>
2022-02-02 16:44:29 -0600

    Editando tarea 1

--- Ejercicios/Tarea1/tarea1.cpp
+++ Ejercicios/Tarea1/tarea1.cpp
@@ -7,20 +7,20 @@ using namespace std;
    
    int A,B, suma = 0, resta = 0, multiplicacion = 0, division = 0;
  
-    cout<< "Ingrese el valor de A:";
-    cin>>A;
-    cout<< "Ingrese el valor de B:"; 
-    cin>>B;
+    cout << "Ingrese el valor de A:"; 
+    cin >> A;
+    cout << "Ingrese el valor de B:"; 
+    cin >> B;
     
     suma = A + B;
     resta = A - B;
     multiplicacion = A * B;
     division = A / B;
     
-    cout << "La suma es:"<< suma <<endl;
-    cout << "La resta es:"<< resta <<endl;
-    cout << "La multiplicacion es:" << multiplicacion <<endl;
-    cout << "La division es:" << division <<endl;
+    cout << "La suma de A+B es:"<< suma << endl;
+    cout << "La resta de a-b es:"<< resta << endl;
+    cout << "La multiplicacion es:" << multiplicacion << endl;
+    cout << "La division es:" << division << |endl;
     
 
 
