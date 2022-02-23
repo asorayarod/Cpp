2c251d1d207dc64787e848df53ec2a4f915acb6c
Andrea Rodriguez <arodriguezi@unah.hn>
2022-02-22 22:25:54 -0600

    agregando ejercico for

---
+++ Ejercicios/ejercicio22 -ciclo for/ciclofor.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char** argv){
+
+    for (int indice = 1; indice<=10; indice ++ )
+{
+    cout << "Valor de la variable indice" << indice << endl;
+
+}
+
+int a = 0;
+
+a= a + 1;
+a++;
+a--;
