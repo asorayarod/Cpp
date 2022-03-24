98f51b68da88a8eb145dad8ac7ececd1c747fa03
asorayarod <98860826+asorayarod@users.noreply.github.com>
2022-03-23 22:27:44 -0600

    Tarea 8
    RaizCuadrada.cpp

---
+++ Ejercicios/Tarea 8 RaizCuadrada.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main ()
+{
+	double a,raiz;
+	
+	cout<<"INGRESE UN NUMERO"<<endl;
+	cin>>a;
+	
+	raiz = sqrt(a);
+	
+	cout<<"LA RAIZ CUADRADA DE" <<a<<  "ES:"  <<raiz;
+	
+	return 0;
+}
